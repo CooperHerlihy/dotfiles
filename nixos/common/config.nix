46132ecc5001c7e2fdd4b364c345ed9c07737645
CooperHerlihy <cooper.herlihy@gmail.com>
2026-06-27 21:39:17 -0400

    added font support for cjk characters

--- nixos/common/config.nix
+++ nixos/common/config.nix
@@ -21,6 +21,9 @@
 
     fonts.packages = with pkgs; [
         nerd-fonts.jetbrains-mono
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-color-emoji
     ];
 
     environment.systemPackages = with pkgs; [
