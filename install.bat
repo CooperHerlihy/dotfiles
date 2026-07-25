8a63c3dd722f0f82e6673bd2aa6981693acbdd2c
CooperHerlihy <cooper.herlihy@gmail.com>
2026-07-25 10:08:15 -0400

    updated windows install script

--- install.bat
+++ install.bat
@@ -7,7 +7,13 @@ if exist "%USERPROFILE%\scripts" (
 )
 mklink /j "%USERPROFILE%\scripts" "scripts-win"
 
-if exist "%APPDATA%.emacs.d" (
+if exist "%APPDATA%\.emacs.d" (
     rmdir /s /q "%ADDPATA%\.emacs.d"
 )
 mklink /j "%APPDATA%\.emacs.d" "emacs"
+
+
+if exist "%LOCALAPPDATA%\nvim" (
+    rmdir /s /q "%LOCALADDPATA%\nvim"
+)
+mklink /j "%LOCALAPPDATA%\nvim" "nvim"
